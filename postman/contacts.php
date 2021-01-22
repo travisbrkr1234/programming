1610aede106161b1455aa7ddaba683b814958236
travisbrkr1234 <travisbrkr1234@users.noreply.github.com>
2021-01-22 00:54:10 -0700

    bug fixes - make it legit bro

--- postman/contacts.php
+++ postman/contacts.php
@@ -1,38 +1,50 @@
 <?php
- $servername = "localhost";
- $username = "";
- $password = "";
- $dbname = "storee";
- $tableName = "contact";
- $databaseConnection;
- 
- if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "storee";
+$tableName = "contact";
+$databaseConnection;
+
+      //Check post Verb for GET
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
     $databaseConnection = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($databaseConnection->connect_error) {
-        die("Connection failed: " . $databaseConnection->connect_error);
+    // Returns either a successful connection pool OR an error
+
+    $dataBaseConnectionError = $databaseConnection->connect_error;
+
+    if (isset($dataBaseConnectionError)) {
+        die("Connection failed: " . $$dataBaseConnectionError);
     }
 
-    $sql = "SELECT * FROM $tableName";
-    $result = $databaseConnection->query($sql);
+    $selectStatement = "SELECT * FROM $tableName ORDER BY id DESC";
+    $result = $databaseConnection->query($selectStatement);
     $results = [];
     if ($result->num_rows > 0 && $result->num_rows < 500) {
         while($row = $result->fetch_assoc()) {
             array_push($results, $row);
         }
-        if (null == $_GET['api'] || (isset($_GET['api']) && htmlspecialchars($_GET['api']) == 'false')) {
+        
+        //check GET parameter for api not NULL and fale
+        $apiIsNull = null == $_GET['api'];
+        $apiIsFalse = (isset($_GET['api']) && htmlspecialchars($_GET['api']) == 'false');
+        $doThisCodeBlock = $apiIsNull || $apiIsFalse;
+        if ($doThisCodeBlock) {
             foreach ($results as $row) {
                 echo "<br> id: ". $row['id'] . " " . $row['first_name'] . " " . $row['email'];
             }
+            // Create new variable GET parameter of true
         } else if (isset($_GET['api']) && htmlspecialchars($_GET['api']) == 'true') {
             echo json_encode($results);
         }
+        //Cant find anything return error message
     } else {
         echo "No resutls found for table: " . $tableName . " check to see if it exists in database " . $databaseConnection;
     }
-
+    //End Connection
     $databaseConnection->close();
 } else {
     echo "HTTP method not supported, please use GET";
 }
+
 ?>
